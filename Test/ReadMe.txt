b335d4e52a88a48ef66bf571ff3d2d9a17335498
wPgg1es <wpggles@hotmail.com>
2016-02-22 00:42:51 +0800

    Add test project.

---
+++ Test/ReadMe.txt
@@ -0,0 +1,6 @@
+当前文件夹中有两个工程VS和Codeblocks的工程。
+主要的文件：
+Test.cbp 是Codeblocks的工程文件。
+Test.sln 是VS的工程文件(准确说是VS2015的工程文件)。
+test.db 是测试用的数据库，可以通过其他的SQLite工具查看其中的数据。
+Test.cpp 是主要的测试代码。
---
+++ Test/Test.cpp
@@ -0,0 +1,117 @@
+// Test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include <iostream>
+#include "../SQLiteMiniReader/SQLiteMiniReader.h"
+using namespace std;
+
+BOOL ProcTable(SQLiteTable * table, void * pVoid)
+{
+	cout << "GetTableName : " << table->GetTableName() << endl;
+	return TRUE;
+}
+
+BOOL ProcCell(SQLiteRecord * record, void * pVoid)
+{
+	cout << "Record count : " << record->GetFieldCount() << endl;
+	for (UINT i = 0; i < record->GetFieldCount(); ++i)
+	{
+		cout << record->GetFieldName(i) << "(" << record->GetFeildTypeName(i) << ") : ";
+
+		switch (record->GetFeildSimilarType(i))
+		{
+			case SDT_BOOLEAN:
+				cout << record->GetBoolean(i);
+				break;
+			case SDT_INTEGER:
+				cout << record->GetInteger(i);
+				break;
+			case SDT_REAL:
+				cout << record->GetReal(i);
+				break;
+			case SDT_TEXT:
+			{
+				char * tmpChars = NULL;
+				cout << "(" << record->GetText(i, tmpChars) << ") ";
+				if (tmpChars)
+				{
+					cout << tmpChars;
+					delete[] tmpChars;
+				}
+				break;
+			}
+			case SDT_BLOB:
+			{
+				BYTE * tmpBytes = NULL;
+				cout << "(" << record->GetBLOB(i, tmpBytes) << ")BLOB";
+				if (tmpBytes)
+				{
+					delete[] tmpBytes;
+				}
+				break;
+			}
+			case SDT_DBNULL:
+				cout << "SDT_DBNULL";
+				break;
+			case SDT_UNKNOWN:
+				if (!_stricmp("datetime", record->GetFeildTypeName(i)))
+				{
+					char * tmpChars = NULL;
+					cout << "(" << record->GetText(i, tmpChars) << ") ";
+					if (tmpChars)
+					{
+						cout << tmpChars;
+						delete[] tmpChars;
+					}
+				}
+				else
+				{
+					cout << "SDT_UNKNOWN";
+				}
+			break;
+		}
+		cout << endl;
+	}
+	cout << "RowID : " << record->GetRowID() << endl;
+	return TRUE;
+}
+
+int main()
+{
+	SQLiteMiniReader sqliteReader("test.db");
+
+	SQLiteFileHeader * info = sqliteReader.GetFileInfo();
+	cout << "info.GetHeaderString() : " << info->GetHeaderString() << endl;
+	cout << "info.GetPageSize() : " << info->GetPageSize() << endl;
+
+	cout << "info.GetFileFormatWriteVersion() : " << info->GetFileFormatWriteVersion() << endl;
+	cout << "info.GetFileFormatReadVersion() : " << info->GetFileFormatReadVersion() << endl;
+	cout << "info.GetFileChangeCount() : " << info->GetFileChangeCount() << endl;
+	cout << "info.GetPageCount() : " << info->GetPageCount() << endl;
+	cout << "info.GetTotalFreelistPages() : " << info->GetTotalFreelistPages() << endl;
+	cout << "info.GetSchemaCookie() : " << info->GetSchemaCookie() << endl;
+	cout << "info.GetSchemaFormatNumber() : " << info->GetSchemaFormatNumber() << endl;
+	cout << "info.GetDefaultPageCacheSize() : " << info->GetDefaultPageCacheSize() << endl;
+	cout << "info.GetRootPageNumber() : " << info->GetRootPageNumber() << endl;
+	cout << "info.GetTextEncoding() : " << info->GetTextEncoding() << endl;
+	cout << "info.GetUserVersion() : " << info->GetUserVersion() << endl;
+	cout << "info.GetIncrementalVacuumMode() : " << info->GetIncrementalVacuumMode() << endl;
+	cout << "info.GetApplicationID() : " << info->GetApplicationID() << endl;
+	cout << "info.GetVersionValidForNumber() : " << info->GetVersionValidForNumber() << endl;
+	cout << "info.GetVersion() : " << info->GetVersion() << endl;
+	cout << "info.GetMajorVersion() : " << info->GetMajorVersion() << endl;
+	cout << "info.GetMinorVersion() : " << info->GetMinorVersion() << endl;
+	cout << "info.GetReleaseNumber() : " << info->GetReleaseNumber() << endl;
+
+	cout << "------------------------------" << endl;
+
+	sqliteReader.QueryTable(ProcTable, NULL);
+
+	cout << "------------------------------" << endl;
+
+	sqliteReader.QueryRecord("userInfo", ProcCell, NULL);
+
+	getchar();
+	return 0;
+}
+
